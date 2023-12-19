f0aacd7882ba216e1786d447f9cf2dab4b664057
DimRev <dimarev444@gmail.com>
2023-12-19 14:30:13 +0200

    miss-bug

---
+++ server.js
@@ -0,0 +1,86 @@
+import express from 'express'
+import { bugService } from './service/bugs.service.js'
+import { loggerService } from './service/logger.service.js'
+import cookieParser from 'cookie-parser'
+
+const app = express()
+
+app.use(express.static('public'))
+app.use(cookieParser())
+
+
+//Get Bugs
+app.get('/api/bug', (req, res) => {
+  bugService
+    .query()
+    .then((bugs) => {
+      res.send(bugs)
+    })
+    .catch((err) => {
+      loggerService.error('Cannot get bugs', err)
+      res.status(404).send('cannot get bugs')
+    })
+})
+
+//Add Bug
+
+//Edit Bug
+app.get('/api/bug/save', (req, res) => {
+  const { title, description, severity, _id } = req.query
+  const bugToSave = { title, description, severity, _id }
+  bugService
+    .save(bugToSave)
+    .then((bug) => res.send(bug))
+    .catch((err) => {
+      loggerService.error('Cannot save bug', err)
+      res.status(404).send('Cannot save bug')
+    })
+})
+
+//
+app.get('/api/bug/:bugId', (req, res) => {
+  const bugId = req.params.bugId
+  bugService
+    .getById(bugId)
+    .then((bug) => {
+      handleCookies(req, res, bug)
+    })
+    .catch((err) => {
+      loggerService.error('Cannot get bug', err)
+      res.status(404).send('Cannot get bug')
+    })
+})
+
+app.get('/api/bug/:bugId/remove', (req, res) => {
+  const bugId = req.params.bugId
+  bugService
+    .remove(bugId)
+    .then((bug) => res.send(bug))
+    .catch((err) => {
+      loggerService.error('cannot remove bug', err)
+      res.status(404).send('Cannot remove bug')
+    })
+})
+
+app.listen(3030, () => {
+  console.log('server ready at port 3030')
+})
+
+function handleCookies(req, res, bug) {
+  let visitedBugs = req.cookies.visitedBugs
+    ? JSON.parse(req.cookies.visitedBugs)
+    : []
+
+  const idIdx = visitedBugs.indexOf(bug._id)
+
+  if (visitedBugs.length > 3) return res.status(401).send('Wait for a bit')
+
+  if (idIdx === -1) {
+    visitedBugs.push(bug._id)
+    visitedBugs = JSON.stringify(visitedBugs)
+    res.cookie('visitedBugs', visitedBugs, { maxAge: 7000 })
+  }
+
+  res.send(bug)
+}
+
---
+++ service/bugs.service.js
@@ -0,0 +1,56 @@
+import fs from 'fs'
+import { utilService } from './utils.service.js'
+
+export const bugService = {
+  query,
+  getById,
+  remove,
+  save,
+}
+
+const bugs = utilService.readJsonFile('data/bugs.json')
+
+function query() {
+  return Promise.resolve(bugs)
+}
+
+function getById(bugId) {
+  const bug = bugs.find((bug) => {
+    return bug._id === bugId
+  })
+  if (!bug) return Promise.reject('Bug dosent exist!')
+
+  return Promise.resolve(bug)
+}
+
+function remove(bugId) {
+  const bugIdx = bugs.findIndex((bug) => bug._id === bugId)
+  bugs.splice(bugIdx, 1)
+  return _saveBugsToFile()
+}
+
+function save(bug) {
+  bug.createdAt = Date.now()
+  if (bug._id) {
+    const bugIdx = bugs.findIndex((currBug) => currBug._id === bug._id)
+    bugs[bugIdx] = bug
+  } else {
+    bug._id = utilService.makeId()
+    bugs.unshift(bug)
+  }
+
+  return _saveBugsToFile().then(() => bug)
+}
+
+function _saveBugsToFile() {
+  return new Promise((resolve, reject) => {
+    const data = JSON.stringify(bugs, null, 2)
+    fs.writeFile('data/bugs.json', data, (err) => {
+      if (err) {
+        console.log(err)
+        return reject(err)
+      }
+      resolve()
+    })
+  })
+}
