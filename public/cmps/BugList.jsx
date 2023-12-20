1f1050f4c987cce2352dde1205eee50fa03c9af9
DimRev <dimarev444@gmail.com>
2023-12-20 17:47:00 +0200

    Fix: const user was outside the BugList cmp function

--- public/cmps/BugList.jsx
+++ public/cmps/BugList.jsx
@@ -3,8 +3,9 @@ const { Link } = ReactRouterDOM
 import { BugPreview } from './BugPreview.jsx'
 import { userService } from '../services/user.service.js'
 
-const user = userService.getLoggedinUser()
 export function BugList({ bugs, onRemoveBug, onEditBug }) {
+  const user = userService.getLoggedinUser()
+
   if (!bugs) return <div>Loading...</div>
   return (
     <ul className="bug-list">
