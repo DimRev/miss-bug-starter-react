97b0330c4bca100806ec1488e3aa44d753f8c07f
DimRev <dimarev444@gmail.com>
2023-12-20 15:47:57 +0200

    Add: Added LoginSignup cmp to AppHeader and a user.service to handle it's logic

--- public/cmps/AppHeader.jsx
+++ public/cmps/AppHeader.jsx
@@ -1,16 +1,49 @@
-const {NavLink} = ReactRouterDOM
-const {useEffect} = React
+const { NavLink, useNavigate, Link } = ReactRouterDOM
+const { useEffect, useState } = React
 
-import {UserMsg} from './UserMsg.jsx'
+import { LoginSignup } from './LoginSignup.jsx'
+import { UserMsg } from './UserMsg.jsx'
+
+import { userService } from '../services/user.service.js'
 
 export function AppHeader() {
   useEffect(() => {
-    // component did mount when dependancy array is empty
+    // component did mount when dependency array is empty
   }, [])
+
+  const navigate = useNavigate()
+
+  const [user, setUser] = useState(userService.getLoggedinUser())
+
+  function onLogout() {
+    userService
+      .logout()
+      .then(() => {
+        onSetUser(null)
+      })
+      .catch((err) => {
+        showErrorMsg('OOPs try again')
+      })
+  }
+
+  function onSetUser(user) {
+    setUser(user)
+    navigate('/')
+  }
 
   return (
     <header>
       <UserMsg />
+      {user ? (
+        <section>
+          <Link to={`/user/${user._id}`}>Hello {user.fullname}</Link>
+          <button onClick={onLogout}>Logout</button>
+        </section>
+      ) : (
+        <section>
+          <LoginSignup onSetUser={onSetUser} />
+        </section>
+      )}
       <nav>
         <NavLink to="/">Home</NavLink> |<NavLink to="/bug">Bugs</NavLink> |
         <NavLink to="/about">About</NavLink>
