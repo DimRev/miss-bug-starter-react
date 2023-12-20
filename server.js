4ad27a62de9ffe577cb79868c0287efb49c489ee
DimRev <dimarev444@gmail.com>
2023-12-20 14:23:10 +0200

    Add: Added userDB and userService to the backend

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+import path from 'path'
 import express from 'express'
-import { bugService } from './service/bugs.service.js'
-import { loggerService } from './service/logger.service.js'
 import cookieParser from 'cookie-parser'
+
+import { bugService } from './service/bugs.service.js'
+import { userService } from './service/user.service.js'
+import { loggerService } from './service/logger.service.js'
 
 const app = express()
 
@@ -85,9 +88,61 @@ app.delete('/api/bug/:bugId', (req, res) => {
     })
 })
 
-app.listen(3030, () => {
-  console.log('server ready at port 3030')
+// AUTH API
+app.get('/api/user', (req, res) => {
+  userService.query()
+    .then((users) => {
+      res.send(users)
+    })
+    .catch((err) => {
+      console.log('Cannot load users', err)
+      res.status(400).send('Cannot load users')
+    })
 })
+
+app.post('/api/auth/login', (req, res) => {
+  const credentials = req.body
+  userService.checkLogin(credentials)
+    .then(user => {
+      if (user) {
+        const loginToken = userService.getLoginToken(user)
+        res.cookie('loginToken', loginToken)
+        res.send(user)
+      } else {
+        res.status(401).send('Invalid Credentials')
+      }
+    })
+})
+
+app.post('/api/auth/signup', (req, res) => {
+  const credentials = req.body
+  userService.save(credentials)
+    .then(user => {
+      if (user) {
+        const loginToken = userService.getLoginToken(user)
+        res.cookie('loginToken', loginToken)
+        res.send(user)
+      } else {
+        res.status(400).send('Cannot signup')
+      }
+    })
+})
+
+app.post('/api/auth/logout', (req, res) => {
+  res.clearCookie('loginToken')
+  res.send('logged-out!')
+})
+
+
+
+app.get('/**', (req, res) => {
+  res.sendFile(path.resolve('public/index.html'))
+})
+
+const PORT = 3030
+app.listen(PORT, () =>
+  loggerService.info(`Server listening on port http://127.0.0.1:${PORT}/`)
+)
 
 function handleCookies(req, res, bug) {
   let visitedBugs = req.cookies.visitedBugs
