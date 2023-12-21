4c08600a052e1f781c220638029185d661655541
DimRev <dimarev444@gmail.com>
2023-12-21 09:41:48 +0200

    Style : Added imgs to bugs + description in BugDetails

--- public/cmps/BugPreview.jsx
+++ public/cmps/BugPreview.jsx
@@ -4,7 +4,7 @@ export function BugPreview({bug}) {
 
     return <article className="bug-preview">
         <h4>{bug.title}</h4>
-        <h1>🐛</h1>
+        <img src={`https://api.dicebear.com/7.x/rings/svg?seed=${bug.title}`} alt="bug img" />
         <p>Severity: <span>{bug.severity}</span></p>
         <p>Submitted By:{bug.submittedBy.fullname}</p>
     </article>
--- public/pages/BugDetails.jsx
+++ public/pages/BugDetails.jsx
@@ -4,29 +4,36 @@ const { Link, useParams } = ReactRouterDOM
 import { bugService } from '../services/bug.service.js'
 import { showErrorMsg } from '../services/event-bus.service.js'
 
-
 export function BugDetails() {
+  const [bug, setBug] = useState(null)
+  const { bugId } = useParams()
 
-    const [bug, setBug] = useState(null)
-    const { bugId } = useParams()
+  useEffect(() => {
+    bugService
+      .getById(bugId)
+      .then((bug) => {
+        setBug(bug)
+      })
+      .catch((err) => {
+        showErrorMsg('Cannot load bug')
+      })
+  }, [])
 
-    useEffect(() => {
-        bugService.getById(bugId)
-            .then(bug => {
-                setBug(bug)
-            })
-            .catch(err => {
-                showErrorMsg('Cannot load bug')
-            })
-    }, [])
-
-    if (!bug) return <h1>loadings....</h1>
-    return bug && <section className='bug-details-section main-section'>
-        <h3>Bug Details 🐛</h3>
+  if (!bug) return <h1>loadings....</h1>
+  return (
+    bug && (
+      <section className="bug-details-section main-section">
+        <img
+          src={`https://api.dicebear.com/7.x/rings/svg?seed=${bug.title}`}
+          alt="bug img"
+        />
         <h4>{bug.title}</h4>
-        <p>Severity: <span>{bug.severity}</span></p>
+        <p>{bug.description}</p>
+        <p>
+          Severity: <span>{bug.severity}</span>
+        </p>
         <Link to="/bug">Back to List</Link>
-    </section>
-
+      </section>
+    )
+  )
 }
-
